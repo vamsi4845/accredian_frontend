81232c24f87f5506e3c0d5c50c5c4f8e34495aeb
vamsisai <vamsisai.karumuri@gmail.com>
2024-07-11 21:07:03 +0530

    fix env issue

--- src/components/ReferForm.jsx
+++ src/components/ReferForm.jsx
@@ -21,7 +21,7 @@ export default function ReferForm() {
     };
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_RENDER_API}/api/referrals`, {
+      const response = await fetch(`${import.meta.env.VITE_VERCEL_RENDER_API}/api/referrals`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
